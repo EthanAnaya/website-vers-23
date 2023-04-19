226e9bd530e47f471ecce90b09fe91c948b951f7
EthanAnaya <124911792+EthanAnaya@users.noreply.github.com>
2023-04-19 13:20:11 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function validatePassword() {
+	var password = document.getElementById("password-input").value;
+
+	if (password == "myPassword") {
+		window.location.href = "secret-page.html";
+	} else {
+		document.getElementById("password-form").reset();
+		var errorMessage = document.createElement("p");
+		errorMessage.innerHTML = "Incorrect password. Please try again.";
+		errorMessage.id = "error-message";
+		document.getElementById("password-form").appendChild(errorMessage);
+		event.preventDefault();
+	}
+}
