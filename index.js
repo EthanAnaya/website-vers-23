a313ff36be42e0c7035b43e48379ced5264d303f
EthanAnaya <124911792+EthanAnaya@users.noreply.github.com>
2023-04-19 20:24:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var page = new ABC({
-    "type": "blank", //Blank, blob, or overwrite
-    "url": "https://example.com" //Any url
-})
-page.setType("blank")
-page.setUrl("https://historicalanime.org/")
-console.log(page.getCode())
-page.open()
+
